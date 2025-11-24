55c1aaaa4b282d2da90727f0627faba6a16f3eba
Carlos Eduardo Rodriguez <carlos96_007@hotmail.com>
2025-11-24 10:18:07 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  titulo = 'Portal Capacitaciones GFT Tecnologies';
+  titulo = 'Portal Capacitaciones GFT Technologies';
 }
